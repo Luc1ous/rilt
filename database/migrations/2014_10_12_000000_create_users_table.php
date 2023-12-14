a7d87697433a2cf2007a17747c37c4fee2981879
Luc1ous <ardianilyas@gmail.com>
2023-12-14 19:59:16 +0700

    add column image in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->string('fullname');
             $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->string('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('image')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
