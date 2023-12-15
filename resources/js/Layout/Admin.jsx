500fe038d7fa7bf8ea90aa79b67ad2fb44ed7242
Luc1ous <ardianilyas@gmail.com>
2023-12-15 11:49:34 +0700

    add hompage menu and auth email

--- resources/js/Layout/Admin.jsx
+++ resources/js/Layout/Admin.jsx
@@ -1,9 +1,10 @@
 import { Head, Link, usePage } from '@inertiajs/inertia-react'
-import { ClipboardIcon, DashboardIcon, LockOpen1Icon, MixIcon, PersonIcon, RocketIcon } from '@radix-ui/react-icons'
+import { ClipboardIcon, DashboardIcon, HomeIcon, LockOpen1Icon, MixIcon, PersonIcon, RocketIcon } from '@radix-ui/react-icons'
 import React from 'react'
 
 export default function Admin({ children, title }) {
   const { url } = usePage()
+  const { auth } = usePage().props
   return (
     <>
       <Head>
@@ -29,12 +30,12 @@ export default function Admin({ children, title }) {
                   <div className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] bg-white shadow-md rounded-lg p-2 dark:bg-gray-800 dark:border dark:border-gray-700" aria-labelledby="hs-dropdown-with-header">
                     <div className="py-3 px-5 -m-2 bg-gray-100 rounded-t-lg dark:bg-gray-700">
                       <p className="text-sm text-gray-500 dark:text-gray-400">Signed in as</p>
-                      <p className="text-sm font-medium text-gray-800 dark:text-gray-300">james@site.com</p>
+                      <p className="text-sm font-medium text-gray-800 dark:text-gray-300">{auth.user.email}</p>
                     </div>
                     <div className="mt-2 py-2 first:pt-0 last:pb-0">
                       <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="#">
-                        <svg className="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/></svg>
-                        Newsletter
+                        <HomeIcon />
+                        Homepage
                       </a>
                     </div>
                   </div>
