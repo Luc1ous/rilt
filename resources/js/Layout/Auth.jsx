f1c5cd7e61f127e117d5c84d90e7d31dc78e0977
Luc1ous <ardianilyas@gmail.com>
2023-12-17 18:42:13 +0700

    change max width for auth layout

--- resources/js/Layout/Auth.jsx
+++ resources/js/Layout/Auth.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Auth({ children }) {
   return (
     <div className='w-full min-h-screen bg-gray-50 flex justify-center items-center'>
-      <div className='w-full max-w-2xl px-6 md:px-0'>
+      <div className='w-full max-w-lg px-6 md:px-0'>
         {children}
       </div>
     </div>
