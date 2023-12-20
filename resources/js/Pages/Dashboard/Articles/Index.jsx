604a9f86eb3b40a19dfcbea049da496d61942829
Luc1ous <ardianilyas@gmail.com>
2023-12-20 19:20:04 +0700

    update articles index page

--- resources/js/Pages/Dashboard/Articles/Index.jsx
+++ resources/js/Pages/Dashboard/Articles/Index.jsx
@@ -4,18 +4,31 @@ import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, Tabl
 import { Button } from '@/Components/ui/button'
 import { Card, CardContent, CardFooter } from '@/Components/ui/card'
 import Pagination from '@/Components/Pagination'
-import { PlusIcon } from '@radix-ui/react-icons'
+import { Link, usePage } from '@inertiajs/inertia-react'
+import { CheckCircledIcon, Cross1Icon, Pencil2Icon, PlusIcon, TrashIcon } from '@radix-ui/react-icons'
+import { Alert, AlertDescription, AlertTitle } from '@/Components/ui/alert'
 
 export default function Index({ articles }) {
+  const { flash } = usePage().props
   return (
     <Admin>
       <Admin.Title>Articles</Admin.Title>
       <Admin.Description>Here is your articles list. You can create, edit, and delete your articles here</Admin.Description>
 
-      <Button className='my-4 flex gap-2'>
-        <PlusIcon />
-        Create new article
-      </Button>
+      <Link className='inline-flex' href='/dashboard/articles/create'>
+        <Button className='my-4 flex gap-2'>
+          <PlusIcon />
+          Create new article
+        </Button>
+      </Link>
+
+      {flash.success && 
+        <Alert className='text-green-600 mb-6'>
+          <CheckCircledIcon />
+          <AlertTitle>Success</AlertTitle>
+          <AlertDescription>{flash.success}</AlertDescription>
+        </Alert>
+      }
       
       <div className='mb-10'>
         <Card>
@@ -37,8 +50,14 @@ export default function Index({ articles }) {
                     <TableCell>{article.title}</TableCell>
                     <TableCell>{article.slug}</TableCell>
                     <TableCell className='flex gap-2'>
-                      <Button>Edit</Button>
-                      <Button variant='destructive'>Delete</Button>
+                      <Link href={`/dashboard/articles/${article.slug}`}>
+                        <Button>
+                            <Pencil2Icon />
+                        </Button>
+                      </Link>
+                      <Button variant='destructive'>
+                        <TrashIcon />
+                      </Button>
                     </TableCell>
                   </TableRow>
                 ))}
