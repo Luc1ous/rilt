9f0abb320891a4bd15af229e967b95a5c0df230e
Luc1ous <ardianilyas@gmail.com>
2023-12-23 20:15:34 +0700

    add auth request

---
+++ app/Http/Requests/SignUpRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SignUpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|unique:users,email',
+            'fullname' => 'required|string|min:2',
+            'username' => 'required|min:5|unique:users,username',
+            'password' => 'required|min:8'
+        ];
+    }
+}
