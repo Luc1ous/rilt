70ef5ec001c3ec973bc70da75fad0199100f1554
Luc1ous <ardianilyas@gmail.com>
2023-12-23 20:47:02 +0700

    update category view when no articles

--- resources/js/Pages/Main/CategoryDetail.jsx
+++ resources/js/Pages/Main/CategoryDetail.jsx
@@ -14,15 +14,26 @@ export default function CategoryDetail({ articles, category }) {
         </CardHeader>
       </Card>
 
-      <div className='my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-        {articles.data.map((article, index) =>(
-          <ArticleCard key={index} article={article} />
-        ))}
-      </div>
+      {articles.data.length == 0 && (
+        <div className='max-w-sm mx-auto my-8'>
+          <img src={'/assets/empty.png'} alt="" />
+          <p className='text-center'>There is no article with category : {category.name}</p>
+        </div>
+      )}
 
-      <div className='w-full my-8'>
-        <Pagination data={articles} />
-      </div>
+      {articles.data.length > 0 && (
+        <>
+          <div className='my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {articles.data.map((article, index) =>(
+              <ArticleCard key={index} article={article} />
+            ))}
+          </div>
+
+          <div className='w-full my-8'>
+            <Pagination data={articles} />
+          </div>
+        </>
+      )}
     </Default>
   )
 }
