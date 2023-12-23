5f0c0487025dc3f5712e3bb64c24110996f82234
Luc1ous <ardianilyas@gmail.com>
2023-12-23 20:40:04 +0700

    update article page when no article show empty image

--- resources/js/Pages/Main/Articles.jsx
+++ resources/js/Pages/Main/Articles.jsx
@@ -9,15 +9,26 @@ export default function Articles({ articles }) {
       <h3 className='text-3xl font-semibold'>Articles page</h3>
       <p className='text-base text-gray-600 leading-loose'>Here is you can read articles about laravel and it's ecosystem.</p>
 
-      <div className='w-full my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-        {articles.data.map((article, index) => (
-          <ArticleCard key={index} article={article} />
-        ))}
-      </div>
+      {articles.data.length == 0 && (
+        <div className='max-w-sm mx-auto'>
+          <img src={`/assets/empty.png`} alt="" />
+          <p className='text-center'>There is no articles right now</p>
+        </div>
+      )}
 
-      <div className='w-full my-8'>
-        <Pagination data={articles} />
-      </div>
+      {articles.data.length > 0 && (
+        <>
+          <div className='w-full my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {articles.data.map((article, index) => (
+              <ArticleCard key={index} article={article} />
+            ))}
+          </div>
+
+          <div className='w-full my-8'>
+            <Pagination data={articles} />
+          </div>
+        </>
+      )}
     </Default>
   )
 }
