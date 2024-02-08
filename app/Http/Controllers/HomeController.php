eae05526ca69d4b16f1bbd7f5e835314b30911a9
Luc1ous <ardianilyas@gmail.com>
2024-02-08 20:02:38 +0700

    check article status (publish or draft)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,30 +4,40 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\Category;
+use Exception;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return inertia('Main/Home');
     }
 
-    public function articles() {
+    public function articles()
+    {
         $articles = Article::with('category')->published()->latest()->paginate(9);
         return inertia('Main/Articles', compact('articles'));
     }
 
-    public function categories() {
+    public function categories()
+    {
         $categories = Category::latest()->get();
         return inertia('Main/Categories', compact('categories'));
     }
 
-    public function showArticle(Article $article) {
-        $article->load(['category', 'author']);
-        return inertia('Main/Article', compact('article'));
+    public function showArticle(Article $article)
+    {
+        if ($article->status == 'published') {
+            $article->load(['category', 'author']);
+            return inertia('Main/Article', compact('article'));
+        } else {
+            abort(404);
+        }
     }
 
-    public function categoryDetail(Category $category) {
+    public function categoryDetail(Category $category)
+    {
         $articles = Article::where('category_id', $category->id)->with('category')->latest()->paginate(9);
         return inertia('Main/CategoryDetail', compact('articles', 'category'));
     }
