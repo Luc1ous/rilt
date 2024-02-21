ee26000e2a6d23c22b2b2061239a944f775b4312
Luc1ous <ardianilyas@gmail.com>
2024-02-21 22:15:10 +0700

    fix: fix article thumbnail ui

--- resources/js/Pages/Main/Article.jsx
+++ resources/js/Pages/Main/Article.jsx
@@ -13,7 +13,7 @@ export default function Article({ article }) {
         <p className='my-8 text-sm md:text-base font-normal text-gray-600'>
           {article.created_at} | Author : <Link href={`/user/${article.author.name}`} className='hover:text-blue-500 hover:underline'>@{article.author.name}</Link>
         </p>
-        <img className='w-full mx-auto aspect-auto rounded-2xl' src="https://images.unsplash.com/photo-1701962541409-e2c1256a8574?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8" alt="" />
+        <img className='w-full mx-auto aspect-auto rounded-2xl' src={article.thumbnail} alt="" />
 
         <p className='whitespace-pre-line text-lg text-gray-700 mt-10'>
           {article.body}
